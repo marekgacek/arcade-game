e4912d8486d33086fc3d8e5e9caf175d1d93d4ff
Marek Gacek <gacekmarek92@gmail.com>
2018-04-24 18:43:19 +0200

    Player class

--- js/app.js
+++ js/app.js
@@ -28,7 +28,43 @@ render() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
+class Player{
+	constructor(x,y){
+		this.x = x;
+		this.y = y;
+		this.avatar ='images/char-boy.png';
+	}
+	update() {
+		this.render();
+	}
+	 render() {
+        ctx.drawImage(Resources.get(this.avatar), this.x, this.y);
+    }
+	handleInput(key){
+		switch (key) {
+			case 'left':
+			if (this.x >0) {
+				this.x -= 100;
+			}
+			break;
+			case 'right':
+			if (this.x <400) {
+				this.x += 100;
+			}
+			break;
+			case 'up':
+			if (this.y >0) {
+				this.y -= 90;
+			}
+			break;
+			case 'down':
+			if (this.y <400) {
+				this.y += 90;
+			}
+			break;
+		}
+	}
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
