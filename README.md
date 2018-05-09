e8a83c967f0dca76c14a563be336ad09aee25f16
Marek Gacek <gacekmarek92@gmail.com>
2018-05-09 15:37:36 +0200

    Readme and score changes

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
-frontend-nanodegree-arcade-game
+#  Arcade Game Project
+
+## Table of Contents
+
+* [Instructions](#instructions)
+* [Install](#install)
+* [Contributing](#contributing)
+
+## Instructions
+
+The goal of this game is to reach top of the board (water place) avoiding enemies. You have 3 lives represented by hearts on the left side of screen. 
+Every time you get into street area your score increase by 5 and when you reach water your score increase by 20. Try to get as many points without losing your lives.
+
+First choose your avatar by clicking and then you can control it by using arrows.
+
+**Good Luck**
+
+## Install
+
+Open this in any browser, I recommend Google Chrome.
+
+## Contributing
+
+### frontend-nanodegree-arcade-game
 ===============================
 
 Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
--- js/app.js
+++ js/app.js
@@ -15,54 +15,54 @@ const playerImages = [
 // Enemies our player must avoid
 class Enemy {
     constructor(x, y) {
-            this.x = x;
-            this.y = y;
-            this.dt = Math.floor(Math.random() * 2 + 1) * speed;
-            this.collision = false;
-            this.w = 101;
-            this.h = 171;
-            // The image/sprite for our enemies, this uses
-            // a helper we've provided to easily load images
-            this.sprite = 'images/enemy-bug.png';
-        }
-        // Update the enemy's position, required method for game
+        this.x = x;
+        this.y = y;
+        this.dt = Math.floor(Math.random() * 2 + 1) * speed;
+        this.collision = false;
+        this.w = 101;
+        this.h = 171;
+        // The image/sprite for our enemies, this uses
+        // a helper we've provided to easily load images
+        this.sprite = 'images/enemy-bug.png';
+    }
+    // Update the enemy's position, required method for game
     update() {
-            this.isCollision();
-            this.isCollisionEnemy();
-            this.x += speed * this.dt;
-            //This condition checks when enemy is out of board 
-            if (this.x >= 520) {
-                this.y = posY[enemyNumber];
-                this.x = posX[enemyNumber];
-                enemyNumber++;
-                if (enemyNumber === 3) {
-                    enemyNumber = 0;
-                }
-                this.dt = Math.floor(Math.random() * 2 + 1) * speed;
+        this.isCollision();
+        this.isCollisionEnemy();
+        this.x += speed * this.dt;
+        //This condition checks when enemy is out of board 
+        if (this.x >= 520) {
+            this.y = posY[enemyNumber];
+            this.x = posX[enemyNumber];
+            enemyNumber++;
+            if (enemyNumber === 3) {
+                enemyNumber = 0;
             }
-
-
-
+            this.dt = Math.floor(Math.random() * 2 + 1) * speed;
         }
-        // Draw the enemy on the screen, required method for game
+
+
+
+    }
+    // Draw the enemy on the screen, required method for game
     render() {
-            ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-        }
-        // Axis-Aligned Bounding Box where width = 70 px and height = 60 px
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+    // Axis-Aligned Bounding Box where width = 70 px and height = 60 px
     isCollision() {
-            if (this.x < player.x + 70 &&
-                this.x + 70 > player.x &&
-                this.y < player.y + 60 &&
-                60 + this.y > player.y) {
-                this.dt = 0; // collision detected!
+        if (this.x < player.x + 70 &&
+            this.x + 70 > player.x &&
+            this.y < player.y + 60 &&
+            60 + this.y > player.y) {
+            this.dt = 0; // collision detected!
 
-                if (hearths.lastElementChild) {
-                    hearths.removeChild(hearths.lastElementChild)
-                }
-                this.collision = true;
+            if (hearths.lastElementChild) {
+                hearths.removeChild(hearths.lastElementChild)
             }
+            this.collision = true;
         }
-        //prevent collision of enemies
+    }
+    //prevent collision of enemies
     isCollisionEnemy() {
         allEnemies.forEach(enemy => {
             if (enemy.x + 20 < this.x + this.w && enemy.x + enemy.w + 20 > this.x && this.y === enemy.y) {
@@ -84,13 +84,13 @@ class Enemy {
 // a handleInput() method.
 class Player {
     constructor(look = 0, x, y) {
-            this.look = this.changeLook(look, playerImages);
-            this.x = x;
-            this.y = y;
-            this.lives = 3;
-            this.points = 0;
-        }
-        //This method change Avatar
+        this.look = this.changeLook(look, playerImages);
+        this.x = x;
+        this.y = y;
+        this.lives = 3;
+        this.points = 0;
+    }
+    //This method change Avatar
     changeLook(look, array) {
         this.look = array[look]
         return this.look;
@@ -103,12 +103,15 @@ class Player {
         ctx.drawImage(Resources.get(this.look), this.x, this.y);
     }
     addPoints() {
-            if (this.y > -25 && this.y < 240) {
-                this.points += 10;
-                score.innerHTML = 'SCORE: ' + player.points;
-            }
+        if (this.y > -25 && this.y < 240) {
+            this.points += 5;
+            score.innerHTML = 'SCORE: ' + player.points;
+        } else if (this.y < -20) {
+            this.points += 20;
+            score.innerHTML = 'SCORE: ' + player.points;
         }
-        //Reset player position after reaching water
+    }
+    //Reset player position after reaching water
     waterLevel() {
         if (this.y < 0) {
             setTimeout(function() {
